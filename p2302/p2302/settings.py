092b5cfe07a6957e5f4ca16c3e237e1cf2e606d2
msed <mohsadki@gmail.com>
2023-02-20 20:55:53 +0100

    commit to github

--- p2302/p2302/settings.py
+++ p2302/p2302/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'p2302.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': 'mysql',
+        'HOST': 'ubuntu221',
+        'PORT': '3306',
     }
 }
 
